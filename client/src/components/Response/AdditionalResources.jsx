ea26c02accda337e8289b8c839e33fec3630fd30
nagendra <narrennagendra@gmail.com>
2024-03-22 16:44:37 +0530

    Fix urls in chat page

--- client/src/components/Response/AdditionalResources.jsx
+++ client/src/components/Response/AdditionalResources.jsx
@@ -1,16 +1,35 @@
 /* eslint-disable react/prop-types */
 export default function AdditionalResources( {additionalResources}) {
+	const truncateUrl = (url, maxLength) => {
+		if (url.length <= maxLength) return url;
+		return url.substring(0, maxLength) + "...";
+	};
+
 	return (
 		<>
 			<section className="section additional-resources">
 				<h2 className="section-title">Additional Resources :-</h2>
 				<ul className="list">
 				{additionalResources.map((additionalResource, index) => (
-						<li className="list-item" key={index}>
+					<li className="list-item" key={index}>
 							{additionalResource[0] !== "" ? (
-								<a href={additionalResource[0]}>{additionalResource[1]}</a>
+								<a
+									href={additionalResource[1]}
+									target="_blank"
+									rel="noopener noreferrer"
+									title={additionalResource[1]}
+								>
+									{truncateUrl(additionalResource[0], 50)}
+								</a>
 							) : (
-								<a href={additionalResource[0]}>{additionalResource[0]}</a>
+								<a
+									href={additionalResource[1]}
+									target="_blank"
+									rel="noopener noreferrer"
+									title={additionalResource[1]}
+								>
+									{truncateUrl(additionalResource[1], 30)}
+								</a>
 							)}
 						</li>
 					))}
--- client/src/components/Response/PreRequisites.jsx
+++ client/src/components/Response/PreRequisites.jsx
@@ -1,5 +1,10 @@
 /* eslint-disable react/prop-types */
 export default function Prerequisites({ prerequisites }) {
+	const truncateUrl = (url, maxLength) => {
+		if (url.length <= maxLength) return url;
+		return url.substring(0, maxLength) + "...";
+	};
+
 	return (
 		<>
 			<section className="section prerequisites">
@@ -8,9 +13,23 @@ export default function Prerequisites({ prerequisites }) {
 					{prerequisites.map((prerequisite, index) => (
 						<li className="list-item" key={index}>
 							{prerequisite[0] !== "" ? (
-								<a href={prerequisite[0]}>{prerequisite[1]}</a>
+								<a
+									href={prerequisite[1]}
+									target="_blank"
+									rel="noopener noreferrer"
+									title={prerequisite[1]}
+								>
+									{truncateUrl(prerequisite[0], 50)}
+								</a>
 							) : (
-								<a href={prerequisite[0]}>{prerequisite[0]}</a>
+								<a
+									href={prerequisite[1]}
+									target="_blank"
+									rel="noopener noreferrer"
+									title={prerequisite[1]}
+								>
+									{truncateUrl(prerequisite[1], 30)}
+								</a>
 							)}
 						</li>
 					))}
